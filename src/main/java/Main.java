6af7529c85f3f536e808f6b245f2f9fda9a090be
diegaoProg <diegobarreto.contas@gmail.com>
2022-05-12 23:15:19 -0300

    Desafio Syngenta Digital 2022

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner dataIn = new Scanner(System.in);
-        String reservationData;
+        String reservationData, newData = "s";
         HotelReservation challenge = new HotelReservation();
 
-        System.out.println("=================================================================================");
-        System.out.println("Favor inserir os dados no formato <tipo_do_cliente>: <data1>, <data2>, <data3>, …");
-        reservationData = dataIn.nextLine();
-        System.out.println("=================================================================================");
-        System.out.println("O nome do hotel mais barato é: " + challenge.getCheapestHotel(reservationData));
-        System.out.println("Pressione ENTER para finalizar");
-        dataIn.next();
+        while (!newData.equalsIgnoreCase("n")) {
+            System.out.println("==========================================================");
+            System.out.println("Favor inserir os dados de entrada do programa no formato");
+            System.out.println("<tipo_do_cliente>: <DTMonYEAR(d_o_w)>, <data2>, <data3>, …");
+            reservationData = dataIn.nextLine();
+            System.out.println("==========================================================");
+            System.out.println("O nome do hotel mais barato é: " + challenge.getCheapestHotel(reservationData));
+            System.out.println("Deseja inserir novos dados? (s/n)");
+            newData = dataIn.nextLine();
+        }
+        dataIn.close();
 
     }
 
